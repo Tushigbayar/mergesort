537e89095f5beb09f74086550ba60bb0915d55ea
tushigbayar <tushigbayrr@gmail.com>
2023-11-10 14:11:58 +0800

    merge

--- msort.c
+++ msort.c
@@ -12,7 +12,9 @@
 */
 int nereer(const Student *a, const Student *b)
 {
-        return strcmp(a->name, b->name) < 0;
+        if(strcmp(a->name, b->name) == 0) return 0;
+        else if(strcmp(a->name, b->name) > 0) return -1;
+        else return 1;
 }
 
 /*
@@ -21,7 +23,9 @@ int nereer(const Student *a, const Student *b)
 */
 int nasaar(const Student *a, const Student *b)
 {
-        /* функцийг гүйцээ */
+        if(a->age > b->age) return -1;
+        else if(a->age == b->age) return 0;
+        else return 1;
 }
 
 /*
@@ -30,8 +34,9 @@ int nasaar(const Student *a, const Student *b)
 */
 int golchoor(const Student *a, const Student *b)
 {
-        /* функцийг гүйцээ */
-	
+        if(a->gpa > b->gpa) return -1;
+        else if(a->gpa == b->gpa) return 0;
+        else return 1;
 }
 
 
@@ -45,8 +50,22 @@ void insertion_sort(Student a[],
                     int lo, int hi,
                     int (*less)(const Student *, const Student *))
 {
-        /* функцийг гүйцээ */	
+              int i,j;
+              Student el;
+              for(i = lo; i <= hi; i ++){
+                j=i;
+                while( j > lo){
+                  if(less(a + j - 1, a + j) < 0){    
+                    el = a[j - 1];
+                    a[j - 1]=a[j];
+                    a[j] = el;
+                    j--;
+                  }
+                  else break;
+                }
+              }
 }
+
 
 /*
   Нийлүүлсэн эрэмбэлэх аргын mege үйлдэл.
@@ -63,6 +82,26 @@ void merge(Student a[],
            int (*less)(const Student *, const Student *))
 {
         /* функцийг гүйцээ */
+        int o ;
+        for(o = lo ; o <= hi; o++){ 
+          aux[o] = a[o] ;
+        }
+        int i = lo ;
+        int j = mid + 1 ;
+        for(o = lo; o <= hi; o++){
+          if (i > mid){
+            a[o] = aux[j++] ;
+          }
+          else if (j > hi){
+            a[o] = aux[i++] ;
+          }
+          else if (less(&aux[j], &aux[i]) == 1){
+            a[o] = aux[j++] ;
+          }
+          else {
+            a[o] = aux[i++] ;
+          }
+        }
 }
 
 
@@ -78,5 +117,15 @@ void mergesort(Student a[],
                int hi,
                int (*less)(const Student *, const Student *))
 {
-        /* функцийг гүйцээ */        
+        if(hi - lo <= CUTOFF){
+          insertion_sort(a,lo,hi,less);
+          return;
+        }
+        int mid;
+
+        mid=lo + (hi - lo)/2;
+        mergesort(a,aux,lo,mid,less);
+        mergesort(a,aux,mid+1,hi,less);
+
+        merge(a,aux,lo,mid,hi,less);   
 }
